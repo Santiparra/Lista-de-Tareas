a993a5e12d0b31856b8cc0510d935406ac254f37
santiparra <122759810+Santiparra@users.noreply.github.com>
2023-02-05 16:32:00 -0300

    capa css, falta media query

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './main.scss';
-import{Todo} from './modules/Tasks'
-import{initDisplay,reloadProjects,todoPanel,refreshDisplay}from './modules/UI'
-import { getTodayTodos, storage } from './modules/storage';
-initDisplay();
-refreshDisplay(storage.get('inbox'));
-reloadProjects();
